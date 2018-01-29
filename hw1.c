60b21997e00005c3b37fb382da86e6029ce9662c
Boris Pisabaj <bpisabaj@bertvm.cs.uic.edu>
2018-01-29 00:34:57 -0600

    adding stuff

--- hw1.c
+++ hw1.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+struct node{
+  long one;
+  long two;
+  long three;
+  char some;
+  short myShort;
+  char some1;
+  char somewho;
+  char somehey;
+  char somewhat;
+};
+
+
 
 int main(){
-  printf("bpisab3\n");
+  printf("%s%d\n", "bpisab",3);
   return 0;
+
 }
+
+void all(){
+  return;
+}
+
+int your;
+
+void cs361(){
+  return;
+}
+
+char are = 'a';
+
+int belong;
+
+int to = 0;
+
+struct node us;
+
+
